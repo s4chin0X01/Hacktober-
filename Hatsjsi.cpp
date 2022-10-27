ead90c891f8070f5260ecbc5eb909f60bc580b0e
s4chin0X01 <116457767+s4chin0X01@users.noreply.github.com>
2022-10-27 13:48:03 +0530

    Create Hatsjsi.cpp

---
+++ Hatsjsi.cpp
@@ -0,0 +1,48 @@
+# include<iostream>
+
+    using namespace std;
+
+    int glo = 33;
+
+    void sum(){
+
+    //cout<<"The valuse of Glo is = "<<glo;
+
+    }
+
+    int main(){
+
+    //int glo = 433;
+
+    int glo = 43;
+
+    int a =2;
+
+    int b =5;
+
+    float pi = 3.14;
+
+    char c = 'U';
+
+    bool is_true = true;
+
+    sum();
+
+    cout<<"This is base 4 ";
+
+    cout<<"\nThe value of Glo is = "<<glo;
+
+    cout<<"\nThe value of a = "<<a;
+
+    cout<<"\nThe value of b = "<<b;
+
+    cout<<"\nThe value of c = "<<c;
+
+    cout<<"\nThe value of pi = "<<pi;
+
+    cout<<"\nThe Answer is "<<is_true;
+
+        return 0;
+
+     }
+
